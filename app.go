d82f56f944f597429317524794c8b13e73c34461
Oriol Flors Mas <oriol.flors@visma.com>
2025-09-12 16:23:16 +0200

    Added possibility to use telegram bot api

--- app.go
+++ app.go
@@ -1,5 +1,6 @@
-// TODO static files from go-sqlite-http-template
-// TODO template files from borses.git
+// TODO migrate projects temperatures, borses, monitoring... to validate that
+// the design applies to enough use cases, and check the implementation lines
+// reduction achieved
 package app
 
 import (
@@ -9,10 +10,15 @@ import (
 	"log"
 	"os"
 	"strings"
+
+	telegram "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 var (
-	db *sql.DB
+	db  *sql.DB
+	bot *telegram.BotAPI
+
+	TELEGRAM_CHAT int64
 )
 
 func Init(options ...Option) (err error) {
@@ -24,7 +30,6 @@ func Init(options ...Option) (err error) {
 	return nil
 }
 
-// TODO could also init Telegram, Redis...
 func InitSQL(migrationFiles embed.FS) Option {
 	return func() (err error) {
 		if db, err = initSQL(migrationFiles); err != nil {
@@ -38,6 +43,15 @@ func InitTemplates(templateFiles embed.FS) Option {
 	return func() error {
 		if err := initTemplates(templateFiles); err != nil {
 			return fmt.Errorf("could not initialize templates: %w", err)
+		}
+		return nil
+	}
+}
+
+func InitTelegram(token string, chat int64) Option {
+	return func() (err error) {
+		if bot, err = initTelegram(token, chat); err != nil {
+			return fmt.Errorf("could not initialize telegram: %s", err)
 		}
 		return nil
 	}
@@ -70,4 +84,17 @@ func execute(args []string, commands ...Command) {
 	}
 
 	log.Fatalf("Unknown command «%s»."+options, args[0])
+}
+
+func initTelegram(token string, chat int64) (*telegram.BotAPI, error) {
+	TELEGRAM_CHAT = chat
+	return telegram.NewBotAPI(token)
+}
+
+func SendTelegram(text string) error {
+	msg := telegram.NewMessage(TELEGRAM_CHAT, text)
+	if _, err := bot.Send(msg); err != nil {
+		return fmt.Errorf("could not send: %w", err)
+	}
+	return nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/oriolf/simple-app
 
 go 1.25.0
 
-require github.com/mattn/go-sqlite3 v1.14.32 // indirect
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.32 // indirect
+)
